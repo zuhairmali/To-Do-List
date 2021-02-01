0b775bc3d0254085dd136946708e0305265a17ba
Zuhair Ali <zuhairali20003@gmail.com>
2021-02-01 10:50:16 -0600

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Student on 2/1/21.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
